34abf9471952f30ff076cc0e22d18cb16e9bb453
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2016-08-29 12:13:15 +0900

    [add]とらちゃん描画

--- Killmin/main.cpp
+++ Killmin/main.cpp
@@ -1,5 +1,6 @@
 #include "System.h"
 #include "Camera.h"
+#include "Testiger.h"
 //-----------------------------------------------------------------------------
 // グローバル変数。
 //-----------------------------------------------------------------------------
@@ -10,7 +11,7 @@ D3DXVECTOR4				g_diffuseLightColor[LIGHT_NUM];		//ライトの色。
 D3DXVECTOR4				g_ambientLight;						//環境光
 
 Camera camera;//カメラ
-
+Tiger tora;//とらちゃん
 
 /*!-----------------------------------------------------------------------------
 *@brief	ライトを更新。
@@ -39,6 +40,13 @@ void Init()
 	//ライトを初期化。
 	ZeroMemory(g_diffuseLightDirection, sizeof(g_diffuseLightDirection));
 	ZeroMemory(g_diffuseLightColor, sizeof(g_diffuseLightColor));
+
+	//もでうしょきか
+	tora.Init(g_pd3dDevice);
+	tora.SetPosition(D3DXVECTOR3(0.5f, 0.5f, 0.0f));
+
+	//かえらしょきあ
+	camera.Init();
 }
 //-----------------------------------------------------------------------------
 // Name: 描画処理。
@@ -49,7 +57,17 @@ VOID Render()
 	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 	//シーンの描画開始。
 	g_pd3dDevice->BeginScene();
-
+	/*	-	-	-	-	-	-	-	-	*/
+	tora.Render( //とらちゃん
+		g_pd3dDevice,
+		camera.GetViewMatrix(),
+		camera.GetProjectionMatrix(),
+		g_diffuseLightDirection,
+		g_diffuseLightColor,
+		g_ambientLight,
+		LIGHT_NUM
+		);
+	/*	-	-	-	-	-	-	-	-	*/
 	// シーンの描画終了。
 	g_pd3dDevice->EndScene();
 	// バックバッファとフロントバッファを入れ替える。
@@ -60,13 +78,15 @@ VOID Render()
 -----------------------------------------------------------------------------*/
 void Update()
 {
-	//ライトの更新。
-	UpdateLight();
+	UpdateLight(); //らいと
+	camera.Update();//かめら
+	tora.Update();//とらちゃん
+
 }
 //-----------------------------------------------------------------------------
 //ゲームが終了するときに呼ばれる処理。
 //-----------------------------------------------------------------------------
 void Terminate()
 {
-
+	tora.Release();
 }
