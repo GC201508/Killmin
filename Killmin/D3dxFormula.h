d2fb012f48e2fc2d0aa081124af373bf6433dd3c
Z.Take <kbc15b08@stu.kawahara.ac.jp>
2016-09-07 11:49:59 +0900

    [update]型や式の入力が超絶面倒くさいんでマクロとかクラス使って楽した

---
+++ Killmin/D3dxFormula.h
@@ -0,0 +1,26 @@
+#pragma once
+/*	-	-	-	-	-	-	-	-	-	-	-	*/
+//ベクトルと行列とかの計算が面倒くさいんでまとめますよｫｫｩｰｰ!!!
+/*	-	-	-	-	-	-	-	-	-	-	-	*/
+#include "System.h"
+class dxFor
+{
+public:
+	//ベクトルと行列の乗算
+	static void Vec3Transform(Vec3 *lpV, Matrix *lpM)
+	{
+		Vec3 vTmp = *lpV;
+		lpV->x = vTmp.x * lpM->m[0][0] + vTmp.y * lpM->m[1][0] + vTmp.z * lpM->m[2][0];
+		lpV->y = vTmp.x * lpM->m[0][1] + vTmp.y * lpM->m[1][1] + vTmp.z * lpM->m[2][1];
+		lpV->z = vTmp.x * lpM->m[0][2] + vTmp.y * lpM->m[1][2] + vTmp.z * lpM->m[2][2];
+	}
+
+	//正規化の引数ひとつでええやん
+	static void Vec3Normalize(Vec3 *lpV)
+	{
+		float len = sqrt(lpV->x * lpV->x + lpV->y * lpV->y + lpV->z * lpV->z);
+		lpV->x /= len;
+		lpV->y /= len;
+		lpV->z /= len;
+	}
+};
--- Killmin/System.h
+++ Killmin/System.h
@@ -6,5 +6,18 @@
 #pragma warning( default : 4996 )
 #include <cstdlib>
 
+/*	☆ D3DXってのがまどろっこしい ☆	*/
+#define Vec3 D3DXVECTOR3  //打つのめんどい
+#define Vec4 D3DXVECTOR4  //うつのめんどい
+#define Matrix D3DXMATRIX //宇津のめんこい
+#define onKey GetAsyncKeyState  //どっこい
+#define or ||					//はつこい
+/*	-	-	なんたらデバイス	-	-	*/
 extern LPDIRECT3D9             g_pD3D;
 extern LPDIRECT3DDEVICE9       g_pd3dDevice;
+
+/*	-	いろいろ省いて楽したいがな	-	*/
+#include "D3dxFormula.h" //計算式
+
+/*	-	-	デバックUIの表示	-	-	*/
+const int debuGmodE = 0; //[ON=1][OFF=0]
